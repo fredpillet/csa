2d22835c170cf3f4a4344f075d8b8f74efc961d3
Frédéric Pillet <frederic@pillet.io>
2025-05-07 10:38:02 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -16,6 +16,13 @@ aws_access_key_id = "AKIA475TKMODNDPTLSPB"
 aws_secret_access_key = "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"
 region_name = "us-east-2"
 
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=region_name
+)
+
 def get_secret(secret_name, region_name):
 
     # Create a Secrets Manager client
